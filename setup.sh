67da1485d90846ef7d2b1608769d0130f0e78c07
cooocy <cooocy@foxmail.com>
2023-01-05 18:13:59 +0800

    Install some zsh plugins in seup.sh.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+git clone --depth=1 https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone --depth=1 https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone --depth=1 https://github.com/paulirish/git-open.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/git-open
+
 ln -sf ~/.dotfiles/vimrc ~/.vimrc
 ln -sf ~/.dotfiles/gitconfig ~/.gitconfig
 ln -sf ~/.dotfiles/zshrc ~/.zshrc
